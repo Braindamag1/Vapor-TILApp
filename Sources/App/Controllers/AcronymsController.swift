acfa4a2beaa0cebcbbb1c550c317df727ad54248
YJ.Li <yuanjun.li@beyond-power.com>
2022-05-09 13:34:36 +0800

    feat: Vapor - Create & Query Relationship using Domain Transfer Object

--- Sources/App/Controllers/AcronymsController.swift
+++ Sources/App/Controllers/AcronymsController.swift
@@ -11,61 +11,71 @@ import Vapor
 struct AcronymsController: RouteCollection {
     // register routes
     func boot(routes: RoutesBuilder) throws {
-        let acronymsRoutes = routes.grouped("api","acronyms")
-        acronymsRoutes.post( use: createHandler)
-        acronymsRoutes.get( use: getAllHandler)
+        let acronymsRoutes = routes.grouped("api", "acronyms")
+        acronymsRoutes.post(use: createHandler)
+        acronymsRoutes.get(use: getAllHandler)
         acronymsRoutes.get(":acronymID", use: getHandler)
+        acronymsRoutes.get(":acronymID", "user", use: getUserHandler)
         acronymsRoutes.put(":acronymID", use: updateHandler)
         acronymsRoutes.delete(":acronymID", use: deleteHandler)
         acronymsRoutes.get("search", use: searchHandler)
         acronymsRoutes.get("first", use: getFirstHandler)
         acronymsRoutes.get("sorted", use: sortHandler)
     }
-    
-    
-    //C- create - POST
-    func createHandler(_ req:Request) throws
-    -> EventLoopFuture<Acronym> {
-        let acronym = try req.content.decode(Acronym.self)
+
+    // C- create - POST
+    func createHandler(_ req: Request) throws
+        -> EventLoopFuture<Acronym> {
+        let dto = try req.content.decode(CreateAcronymData.self)
+        let acronym = Acronym(createAcronymData: dto)
         return acronym.save(on: req.db)
             .map { _ in
-                return acronym
+                acronym
             }
     }
-    
-    //R- retrieve -GET
+
+    // R- retrieve -GET
     func getAllHandler(_ req: Request)
-    ->EventLoopFuture<[Acronym]> {
+        -> EventLoopFuture<[Acronym]> {
         Acronym.query(on: req.db)
             .all()
     }
-    
-    func getHandler(_ req:Request)
-    ->EventLoopFuture<Acronym> {
+
+    func getHandler(_ req: Request)
+        -> EventLoopFuture<Acronym> {
         Acronym.find(req.parameters.get("acronymID"), on: req.db)
             .unwrap(or: Abort(.notFound))
-            
     }
-    
-    //U- update - PUT
+
+    func getUserHandler(_ req: Request)
+        -> EventLoopFuture<User> {
+        Acronym.find(req.parameters.get("acronymID"), on: req.db)
+            .unwrap(or: Abort(.notFound))
+            .flatMap { acronym in
+                acronym.$user.get(on: req.db)
+            }
+    }
+
+    // U- update - PUT
     func updateHandler(_ req: Request) throws
-    -> EventLoopFuture<Acronym> {
-        let updateAcronym = try req.content.decode(Acronym.self) // id 是可选的所以不用写
+        -> EventLoopFuture<Acronym> {
+        let updateDTO = try req.content.decode(CreateAcronymData.self)
         return Acronym.find(req.parameters.get("acronymID"), on: req.db)
             .unwrap(or: Abort(.notFound))
             .flatMap { acronym in
-                acronym.short = updateAcronym.short
-                acronym.long = updateAcronym.long
+                acronym.short = updateDTO.short
+                acronym.long = updateDTO.long
+                acronym.$user.id = updateDTO.userID
                 return acronym.save(on: req.db)
                     .map { _ in
-                        return acronym
+                        acronym
                     }
             }
     }
-    
-    //D- delete -DELETE
+
+    // D- delete -DELETE
     func deleteHandler(_ req: Request)
-    ->EventLoopFuture<HTTPStatus> {
+        -> EventLoopFuture<HTTPStatus> {
         Acronym.find(req.parameters.get("acronymID"), on: req.db)
             .unwrap(or: Abort(.notFound))
             .flatMap { acronym in
@@ -73,11 +83,11 @@ struct AcronymsController: RouteCollection {
                     .transform(to: .noContent)
             }
     }
-    
-    //search
-    func searchHandler(_ req:Request) throws
-    ->EventLoopFuture<[Acronym]> {
-        guard let searchItem = req.query[String.self,at:"term"] else {
+
+    // search
+    func searchHandler(_ req: Request) throws
+        -> EventLoopFuture<[Acronym]> {
+        guard let searchItem = req.query[String.self, at: "term"] else {
             throw Abort(.badRequest)
         }
         return Acronym.query(on: req.db)
@@ -87,20 +97,26 @@ struct AcronymsController: RouteCollection {
             }
             .all()
     }
-    
-    //first
+
+    // first
     func getFirstHandler(_ req: Request)
-    -> EventLoopFuture<Acronym> {
+        -> EventLoopFuture<Acronym> {
         Acronym.query(on: req.db)
             .first()
             .unwrap(or: Abort(.notFound))
     }
-    
-    //sort
+
+    // sort
     func sortHandler(_ req: Request)
-    -> EventLoopFuture<[Acronym]> {
+        -> EventLoopFuture<[Acronym]> {
         Acronym.query(on: req.db)
-            .sort(\.$short,.ascending)
+            .sort(\.$short, .ascending)
             .all()
     }
+}
+
+struct CreateAcronymData: Content { // Content is kind like Codable in swift
+    let short: String
+    let long: String
+    let userID: UUID
 }
--- Sources/App/Models/Acronym.swift
+++ Sources/App/Models/Acronym.swift
@@ -20,14 +20,25 @@ final class Acronym: Model {
     @Field(key: "long")
     var long: String
 
+    @Parent(key: "userID")
+    var user:User
     init() {}
 
     init(id: UUID? = nil,
          short: String,
-         long: String) {
-        self.id = id
+         long: String,
+         userID:User.IDValue) {
+        //self.id = id
         self.short = short
         self.long = long
+        self.$user.id = userID
+    }
+    
+    init(createAcronymData: CreateAcronymData) {
+        self.short = createAcronymData.short
+        self.long = createAcronymData.long
+        self.$user.id = createAcronymData.userID
+        // id 是在 save的时候自动创建的?
     }
 }
 
